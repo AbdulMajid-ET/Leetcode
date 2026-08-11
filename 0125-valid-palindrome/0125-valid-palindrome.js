8ed882eeb1b7c12beae235f31e8191f306ecb070
AbdulMajid-ET <majidxxmajjixx@gmail.com>
2026-08-11 12:41:23 +0530

    Time: 6 ms (51.19%), Space: 58.3 MB (46.62%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,9 @@
+var isPalindrome = function(s) {
+    s = s.toLowerCase()
+    s = s.replace(/[^a-z0-9]/g, "")
+
+    let reversed = s.split("").reverse().join("")
+
+    return s === reversed
+};
+
