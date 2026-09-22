526824a564ffa55cdc843e9583251bfec0acc0b6
AbdulMajid-ET <majidxxmajjixx@gmail.com>
2026-09-22 16:02:32 +0530

    Time: 1 ms (76.36%), Space: 55.8 MB (29.17%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,15 @@
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1)
+    let result = []
+
+    for( let num of nums2 ) {
+        if( set1.has(num) ) {
+            result.push(num)
+            set1.delete(num)
+        }
+    }
+
+    return result
+}
+
+intersection([1, 2, 2, 1], [2, 2])
